791fbe67953ba3fcc0b63ab5540ceb1582b3f7bb
Mikhail Abramov <beavisabra@yandex.ru>
2019-06-02 11:37:04 +0400

    Движение

---
+++ Draw/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Draw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1032, 203);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1032, 203);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(1050, 250);
+            this.Name = "Form1";
+            this.Text = "Bus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Draw/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Draw
+{
+    public partial class Form1 : Form
+    {
+        int x = 30;
+        int speed = 10;
+        Bitmap bmp;
+        Graphics graph;
+        Pen pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bmp = new Bitmap(1500, 600);
+            graph = Graphics.FromImage(bmp);
+            pen = new Pen(Color.Red, 4);
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graph.Clear(Color.Transparent);
+
+            graph.DrawLine(pen, x, 60, x, 90);
+            graph.DrawLine(pen, x, 90, x + 30, 90);
+            graph.DrawEllipse(pen, x + 30, 75, 30, 30);
+            graph.DrawLine(pen, x + 60, 90, x + 240, 90);
+            graph.DrawEllipse(pen, x + 240, 75, 30, 30);
+            graph.DrawLine(pen, x + 270, 90, x + 300, 90);
+            graph.DrawLine(pen, x + 300, 90, x + 300, 60);
+            graph.DrawLine(pen, x + 300, 30, x, 30);
+
+            Rectangle rect = new Rectangle(x, 30, 300, 30);
+            Region region = new Region(rect);
+            graph.DrawRectangle(pen, rect);
+            graph.FillRegion(Brushes.Blue, region);
+
+            pictureBox1.Image = bmp;
+
+            x += speed;
+            if (x < 0 || x + 300 > 1000)
+            {
+                speed = -speed;
+            }
+        }
+    }
+}
